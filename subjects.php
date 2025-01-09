b6aedb4591e04679c461933d8b086c2c2f68a695
jazzellee <lee.y@oundleschool.org.uk>
2025-01-09 10:21:01 +0000

    uhyuh

--- subjects.php
+++ subjects.php
@@ -1,14 +1,14 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <title>Subjects</title>
-
+    
 </head>
 <body>
-    <form action="addsubjects.php" method="post">
-    Subject name:<input type="text" name="subjectname"><br>
-    Teacher:<input type="text" name="teacher"><br>
-
-    <input type="submit" value="Add Subject">
+   <form action="addsubjects.php" method="post">
+  Subject name:<input type="text" name="subjectname"><br>
+  Teacher:<input type="text" name="teacher"><br>
+  
+  <input type="submit" value="Add Subject">
 </form>
 <?php
     include_once("connection.php");
@@ -17,8 +17,9 @@
     while ($row=$stmt->fetch(PDO::FETCH_ASSOC))
         {
             #print_r($row);
-            echo($row["subjectname"]."taught by".$row["teacher"]."<br>");
+            echo($row["subjectname"]." taught by ".$row["teacher"]."<br>");
         }
-    ?>
 
+    ?>
+</body>
 </html>
